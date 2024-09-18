f24e9c1dadad639da36eb3525718e8cd5b090d02
Calico Rose Randall <139941083+codebycalico@users.noreply.github.com>
2024-09-18 12:22:42 -0700

    Add OMSI QR, start on button and LED
    Create and add hex code to print OMSI QR code through the AdaFruit Thermal Printer. Started on the definition and initialization of adding the button and LED code.

--- Headwaters uController/src/main.cpp
+++ Headwaters uController/src/main.cpp
@@ -4,8 +4,10 @@
 #include <Adafruit_Thermal.h>
 #include <testTube.h>
 #include <Bounce2.h>
+#include <OMSI_QR.h>
 
 MoToTimer buttonLockoutTimer;
+Bounce2::Button test_button = Bounce2::Button();
 
 uint16_t btnLockTime = 250; 
 
@@ -13,17 +15,39 @@ uint16_t btnLockTime = 250;
 //RX0 TX1
 Adafruit_Thermal printer(&Serial);
 
-void setup() {
+// change to number of LED in the strip
+#define NUM_LEDS 1
+// change to proper pin number
+#define DATA_PIN 3
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-
   printer.printBitmap(tubeWidth, tubeHeight, testTube);
-  if(testbutton.ispressed() && testAllow)
+
+  // initial LED is ? or only blue when button is pushed? 
+  // led[#] CRGB:: ? ;
+  // FastLED.show();
+
+  if(test_button.pressed() && printer.availableForWrite())
   {
-    //print!
+    // LED stream
+    //led[#] = CRGB::Blue;
+    //FastLED.show();
+    // print O^#, isotopes and QR code pointing to OMSI website
+    printer.boldOn();
+    printer.println("O numbers: #");
+    printer.boldOff();
+
+    printer.boldOn();
+    printer.println("Isotope count: ");
+    printer.boldOff();
+
+    printer.printBitmap(QRWidth, QRHeight, OMSI_QR);
   }
-  printer.availableForWrite();
 
 }
