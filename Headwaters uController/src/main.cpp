2cb9d8fa2c0e2151095bf79ca462409e748748bb
Calico Rose Randall <139941083+codebycalico@users.noreply.github.com>
2024-09-24 16:04:26 -0700

    This code works!
    The code now works! When the user pushes the button, it triggers a "stream" visual with the LED strip, and then prints out the data and QR code to the OMSI website from the thermal printer. Updated the OMSI_QR.h to be the correct & working size.

--- Headwaters uController/src/main.cpp
+++ Headwaters uController/src/main.cpp
@@ -6,48 +6,102 @@
 #include <Bounce2.h>
 #include <OMSI_QR.h>
 
-MoToTimer buttonLockoutTimer;
-Bounce2::Button test_button = Bounce2::Button();
+// #include <SoftwareSerial.h>
+// #define TX_PIN 8
+// #define RX_PIN 7
+// SoftwareSerial mySerial(RX_PIN, TX_PIN);
+// Adafruit_Thermal printer(&mySerial);
 
-uint16_t btnLockTime = 250; 
+//uint16_t btnLockTime = 250; 
 
 //setup print to hardware serial
 //RX0 TX1
-Adafruit_Thermal printer(&Serial);
+Adafruit_Thermal printer(&Serial2);
 
-// change to number of LED in the strip
-#define NUM_LEDS 1
+#define BTN_PIN1 0
+Bounce2::Button button1 = Bounce2::Button();
+//MoToTimer buttonLockoutTimer;
+
+#define NUM_LEDS 14
+
 // change to proper pin number
-#define DATA_PIN 3
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+#define LED_PIN1 15
+
+// Define the array of LEDs
+CRGB ledStrip[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, LED_PIN1>(ledStrip, NUM_LEDS);
+
+  FastLED.setBrightness(255);
+  FastLED.show();
+
+  button1.attach(BTN_PIN1, INPUT_PULLUP);
+  // DEBOUNCE INTERVAL IN MILLISECONDS
+  button1.interval(5);
+  // INDICATE THAT THE LOW STATE CORRESPONDS TO PHYSICALLY PRESSING THE BUTTON
+  button1.setPressedState(LOW); 
+
+  Serial.begin(115200);
+  Serial2.begin(9600);
+  //mySerial.begin(9600);
+
+  printer.begin();
+  //ONLY used for debugging or talking to a computer
+  //DELETE after completed
+  while(!Serial);
 }
 
 void loop() {
-  printer.printBitmap(tubeWidth, tubeHeight, testTube);
+  //printer.printBitmap(tubeWidth, tubeHeight, testTube);
+  // must update button every loop
+  button1.update();
 
   // initial LED is ? or only blue when button is pushed? 
   // led[#] CRGB:: ? ;
   // FastLED.show();
 
-  if(test_button.pressed() && printer.availableForWrite())
-  {
-    // LED stream
-    //led[#] = CRGB::Blue;
-    //FastLED.show();
-    // print O^#, isotopes and QR code pointing to OMSI website
-    printer.boldOn();
-    printer.println("O numbers: #");
-    printer.boldOff();
+  if(button1.pressed()){
+    Serial.println("Button was pressed!");
+
+    for(int i = 0; i < NUM_LEDS + 1; i++){
+      fill_solid(ledStrip, i, CRGB::Blue);
+      FastLED.show();
+      delay(100);
+    }
+    for(int i = 0; i < NUM_LEDS + 1; i++){
+      fill_solid(ledStrip, i, CRGB::Black);
+      FastLED.show();
+      delay(100);
+    }
 
     printer.boldOn();
-    printer.println("Isotope count: ");
+    printer.println("Sampling Location 1");
+    printer.println("Data at outlet of Stream 1");
     printer.boldOff();
+    printer.println("O numbers: is this supposed to be oxygen?");
+    printer.println("Isotope count: like so many");
+
 
     printer.printBitmap(QRWidth, QRHeight, OMSI_QR);
+    printer.feed(5);
   }
 
+
+  // if(button1.pressed() && printer.availableForWrite())
+  // {
+  //   // LED stream
+  //   //led[#] = CRGB::Blue;
+  //   //FastLED.show();
+  //   // print O^#, isotopes and QR code pointing to OMSI website
+  //   printer.boldOn();
+  //   printer.println("O numbers: #");
+  //   printer.boldOff();
+
+  //   printer.boldOn();
+  //   printer.println("Isotope count: ");
+  //   printer.boldOff();
+
+  //   printer.printBitmap(QRWidth, QRHeight, OMSI_QR);
+  // }
 }
