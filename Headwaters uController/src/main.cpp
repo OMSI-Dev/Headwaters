28ebb6167befa63e6ca8e75d93d4dc0e78498c49
Calico Rose Randall <139941083+codebycalico@users.noreply.github.com>
2024-09-25 09:38:32 -0700

    Code cleanup
    Cleaned up the code & changed the ints to floats where needed in the printCurrentSample (renamed from printSample)

--- Headwaters uController/src/main.cpp
+++ Headwaters uController/src/main.cpp
@@ -6,16 +6,10 @@
 #include <Bounce2.h>
 #include <OMSI_QR.h>
 
-// #include <SoftwareSerial.h>
-// #define TX_PIN 8
-// #define RX_PIN 7
-// SoftwareSerial mySerial(RX_PIN, TX_PIN);
-// Adafruit_Thermal printer(&mySerial);
-
 //uint16_t btnLockTime = 250; 
 
-//setup print to hardware serial
-//RX0 TX1
+// setup print to hardware serial
+// RX0 TX1
 Adafruit_Thermal printer(&Serial2);
 
 #define BTN_PIN1 0
@@ -23,48 +17,51 @@ Bounce2::Button button1 = Bounce2::Button();
 //MoToTimer buttonLockoutTimer;
 
 #define NUM_LEDS 14
-
-// change to proper pin number
 #define LED_PIN1 15
 
-// Define the array of LEDs
+// define the array of LEDs
 CRGB ledStrip[NUM_LEDS];
 
 void printerTest(){
-    printer.setFont('A');
-    printer.setSize('S');
-    printer.println("This is font A in size S");
-    printer.setSize('M');
-    printer.println("This is font A in size M");
-    printer.setSize('L');
-    printer.println("This is font A in size L");
+  // just a function to print the different font and size options
+  printer.setFont('A');
+  printer.setSize('S');
+  printer.println("This is font A in size S");
+  printer.setSize('M');
+  printer.println("This is font A in size M");
+  printer.setSize('L');
+  printer.println("This is font A in size L");
 
-    printer.setFont('B');
-    printer.setSize('S');
-    printer.println("This is font B in size S");
-    printer.setSize('M');
-    printer.println("This is font B in size M");
-    printer.setSize('L');
-    printer.println("This is font B in size L");
-    printer.feed(3);
+  printer.setFont('B');
+  printer.setSize('S');
+  printer.println("This is font B in size S");
+  printer.setSize('M');
+  printer.println("This is font B in size M");
+  printer.setSize('L');
+  printer.println("This is font B in size L");
+  printer.feed(3);
 }
 
-// last three variables need to be floats
-void printSample(int location, int stream, float isoCount, int temp, int dissolvedOxy){
+void printCurrentSample(int location, int stream, float isoCount, float temp, float dissolvedOxy){
     printer.setFont('B');
     printer.setSize('s');
+
     printer.print("Sample Location: ");
     printer.print(location);
     printer.println();
+
     printer.print("Stream: ");
     printer.print(stream);
     printer.println();
+
     printer.print("Isotope count: ");
     printer.print(isoCount);
     printer.println();
+
     printer.print("Temperature: ");
     printer.print(temp);
     printer.println();
+
     printer.print("Oxygen dissolved: ");
     printer.print(dissolvedOxy);
     printer.println();
@@ -82,40 +79,39 @@ void setup() {
   FastLED.show();
 
   button1.attach(BTN_PIN1, INPUT_PULLUP);
-  // DEBOUNCE INTERVAL IN MILLISECONDS
+  // debounce interval in milliseconds
   button1.interval(5);
-  // INDICATE THAT THE LOW STATE CORRESPONDS TO PHYSICALLY PRESSING THE BUTTON
+  // indicate that the low state corresponds to phsyically pressing the button
   button1.setPressedState(LOW); 
 
   Serial.begin(115200);
   Serial2.begin(9600);
-  //mySerial.begin(9600);
 
   printer.begin();
-  //ONLY used for debugging or talking to a computer
-  //DELETE after completed
+  //ONLY USED FOR DEBUGGING OR TALKING TO A COMPUTER
+  //DELETE BEFORE FINAL
   while(!Serial);
 }
 
 void loop() {
   //printer.printBitmap(tubeWidth, tubeHeight, testTube);
-  // must update button every loop
   button1.update();
 
   if(button1.pressed()){
-    Serial.println("Button was pressed!");
-
+    // create a blue "stream" on the LED strip 
     for(int i = 0; i < NUM_LEDS + 1; i++){
       fill_solid(ledStrip, i, CRGB::Blue);
       FastLED.show();
       delay(100);
     }
+
     for(int i = 0; i < NUM_LEDS + 1; i++){
       fill_solid(ledStrip, i, CRGB::Black);
       FastLED.show();
       delay(100);
     }
-    printSample(1, 1, 20.7, 72, 7);
+
+    printCurrentSample(1, 1, 20.7, 72, 7);
     printQR();
   }
 }
